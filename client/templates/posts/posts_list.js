cdb3b2a77993aead1464f43370d18dd8c8babd83
Dale Pollitt <dale.negativezero@gmail.com>
2015-07-24 11:27:18 -0700

    Add basic posts list template and static data

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Dale on 7/24/2015.
+ */
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+   posts: postsData
+});
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# microscope
+Code from the Discover Meteor tutorial.<br />
+https://www.discovermeteor.com/
